c4cbb3b084101a817727ed4fd9c934a4b86c10d5
beholdr70 <alexsey.krivonogov@gmail.com>
2025-03-19 01:51:48 +0400

    Api topic 1st commit

---
+++ api_file.py
@@ -0,0 +1,2 @@
+def some_api_func(string: str):
+    print(f"this is some api function: {len(string)}")
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from api_file import some_api_func
+
 def some_main_func():
     print('this is main function')
 
 
 if __name__ == '__main__':
+    some_api_func('test string')
     some_main_func()
