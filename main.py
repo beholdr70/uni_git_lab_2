4d542994377b95cea0de26814205758982dd81dd
beholdr70 <alexsey.krivonogov@gmail.com>
2025-03-19 02:05:21 +0400

    Ui topic 2nd commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def some_main_func():
     print('this is main function')
 
 def some_ui_func():
-    print('this is some ui function: 1st edition')
+    print('this is some ui function: 2nd edition')
 
 if __name__ == '__main__':
     some_main_func()
