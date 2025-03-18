3176b85db9cd29b16819761c5305efe0c42b66ef
beholdr70 <alexsey.krivonogov@gmail.com>
2025-03-19 02:07:31 +0400

    Main 2nd commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def some_main_func():
-    print('this is main function')
+def some_main_func(string: str):
+    print(f'this is main function: {some_main_func_dependency(string)}')
 
+def some_main_func_dependency(string: str):
+    return string[::-1]
 
 if __name__ == '__main__':
-    some_main_func()
+    some_main_func('Hello, World!')
