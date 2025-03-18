6645fb03ac667dc35c2c1c25c30ff778c438d589
beholdr70 <alexsey.krivonogov@gmail.com>
2025-03-19 02:01:57 +0400

    Database topic 2nd commit

--- database_file.py
+++ database_file.py
@@ -1,6 +1,8 @@
 def some_database_func():
-    phrase = 'This func call from database file' if __name__ == '__main__' else 'This func call from outside of database file'
+    phrase = 'This func called from database file' if __name__ == '__main__' else 'This func called from outside of database file'
     print(f'This is some database function: {phrase}')
+    if __name__ != '__main__':
+        print(f'The end of database function')
 
 
 if __name__ == '__main__':
