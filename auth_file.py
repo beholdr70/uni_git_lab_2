9158dc3ca3c59bcb2fb727e41cb107e1bcf3211a
beholdr70 <alexsey.krivonogov@gmail.com>
2025-03-19 01:46:29 +0400

    Auth topic 1st commit

---
+++ auth_file.py
@@ -0,0 +1,2 @@
+def some_auth_func(user: str):
+    print(f"this is some auth function: Your username is {user}")
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from auth_file import some_auth_func
+
 def some_main_func():
     print('this is main function')
 
 
 if __name__ == '__main__':
     some_main_func()
+
+    #auth_file test
+    some_auth_func('Username')
