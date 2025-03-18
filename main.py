0ed746c72d22a0cde0e792ac399f98bc04ac18e1
beholdr70 <alexsey.krivonogov@gmail.com>
2025-03-19 02:05:08 +0400

    Ui topic 1st commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def some_main_func():
     print('this is main function')
 
+def some_ui_func():
+    print('this is some ui function: 1st edition')
 
 if __name__ == '__main__':
     some_main_func()
